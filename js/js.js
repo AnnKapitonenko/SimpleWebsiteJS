10493e305b32aeef9a9016e53bf2bfc21a26fd3b
AnnKapitonenko <vishenka030290@gmail.com>
2020-01-26 18:58:37 +0200

    SimpleWebsiteJS - module2

---
+++ js/js.js
@@ -0,0 +1,17 @@
+;(function($){
+
+        $('#intro').slick({
+            dots: true,
+            autoplay: true,
+            speed: 100,
+            fade: true
+        });
+    $('#slider2 .inner').slick({
+        dots: true,
+        slidesToShow: 3,
+        sliderToScroll: 1,
+        autoplay: true,
+        speed: 400
+    });
+
+})(jQuery);
